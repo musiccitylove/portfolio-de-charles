afe9cdb0f903080610a850b41bac15a1a37d9d5a
Charles <charlesdpulliam@gmail.com>
2020-01-24 16:42:39 -0600

    evertyhing functional needs layout work

--- index.js
+++ index.js
@@ -8,5 +8,24 @@ function myFunction() {
   }
 }
 
+function greeting(){
+  let myDate = new Date();
+  let hrs = myDate.getHours();
+
+  let greet='';
+
+  if (hrs < 12)
+    greet = 'Good Morning!';
+  else if (hrs >= 12 && hrs <= 17)
+    greet = 'Good Afternoon!';
+  else if (hrs >= 17 && hrs <= 24)
+    greet = 'Good Evening!';
+
+  document.getElementById('greeting').innerHTML =
+      greet + '<br><img src="/images/ChaChaLogo.jpg" alt="Charles logo" id ="charlesLogo"><br> This is Charles.';
+}
+
+greeting();
+
 
 
